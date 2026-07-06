82af248dff1fca4dd45e3682292d33046f799567
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2026-07-06 19:25:32 -0300

    refactor: retirando o static de partida e passando por paramentro seguindo ordens do professor

--- src/partida.c
+++ src/partida.c
@@ -3,35 +3,27 @@
 #include "../include/jogador_teclado.h"
 #include "../include/tabuleiro.h"
 
-typedef struct {
-    JogadorTeclado jogador1;
-    JogadorTeclado jogador2;
-    Tabuleiro tabuleiro;
-} Partida;
-
-static Partida partida;
-
-void configuraJogadores() {
-    partida.jogador1.tipo = 1;
-    partida.jogador2.tipo = 4;
+void configuraJogadores(Partida *partida) {
+    partida->jogador1.tipo = 1;
+    partida->jogador2.tipo = 4;
 }
 
-void inicia() {
+void inicia(Partida *partida) {
     EstadoJogo resultado = DESCONHECIDO;
     int vez = 1;
 
-    desenha(&partida.tabuleiro);
+    desenha(&partida->tabuleiro);
 
     while (resultado == DESCONHECIDO) {
         if (vez == 1) {
-            joga(partida.jogador1, &partida.tabuleiro);
+            joga(partida->jogador1, &partida->tabuleiro);
             vez = 2;
         } else {
-            joga(partida.jogador2, &partida.tabuleiro);
+            joga(partida->jogador2, &partida->tabuleiro);
             vez = 1;
         }
 
-        resultado = temVencedor(&partida.tabuleiro);
+        resultado = temVencedor(&partida->tabuleiro);
     }
 
     if (resultado == 1) {
